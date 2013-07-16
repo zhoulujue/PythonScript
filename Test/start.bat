ca88dfccad1d4277e385f6d4ca62954f4f37eed0
zhoulujue <zhoulujue@gmail.com>
2013-07-16 15:28:00 +0800

    把root加上了

--- Test/start.bat
+++ Test/start.bat
@@ -1,11 +1,1 @@
-adb root
-adb remount
-adb push ./su.michael /system/xbin/su
-adb shell chmod 06755 /system
-adb shell chmod 06755 /system/xbin/su
-adb uninstall com.michael.words
-adb shell rm /system/app/WordsKeyEvent.apk
-adb push WordsKeyEvent.apk /system/app/
-adb push raw.config /data/data/com.michael.words/files/raw.config
-
-
+adb rootadb remountadb push ./su.michael /system/xbin/suadb shell chmod 06755 /systemadb shell chmod 06755 /system/xbin/suadb uninstall com.michael.wordsadb shell rm /system/app/WordsKeyEvent.apkadb push WordsKeyEvent.apk /system/app/adb push raw.config /data/data/com.michael.words/files/raw.config
